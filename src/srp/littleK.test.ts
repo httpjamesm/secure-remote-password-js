9831c9633b19f1f1b24434a64f363cdaf437cf46
httpjamesm <github@httpjames.space>
2024-11-05 03:42:02 -0500

    feat: equal test

--- src/srp/littleK.test.ts
+++ src/srp/littleK.test.ts
@@ -3,6 +3,7 @@ import { BigInteger } from "jsbn";
 import { hexToBigInt } from "../utils/hex";
 import { knownGroups } from "./srpGroup";
 import { bigIntToBytes } from "../utils/bigint";
+import assert from "assert";
 
 const makeLittleK = (N: BigInteger, g: BigInteger) => {
   const hash = createHash("sha256");
@@ -18,4 +19,7 @@ const k = makeLittleK(
   knownGroups[8192].getN(),
   knownGroups[8192].getGenerator()
 );
-console.log(k.toString());
+assert.deepStrictEqual(
+  k.toString(),
+  "41355140986095207529712434517822669449058164734798422081556677809506232522678"
+);
